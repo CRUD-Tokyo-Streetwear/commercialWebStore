726158afdbd7bf9f281afdfaab214ab7b2901ec4
Rafa <rafaelleotsuka@gmail.com>
2023-11-07 16:02:04 -0300

    cadastro

--- sistema/produto.php
+++ sistema/produto.php
@@ -1,39 +1,75 @@
-
-
-
 <?php
 
-class Produto{
+class Produto
+{ //Cria o objeto produto
 
     private $pdo; //variavel declarada do lado de fora pois sera utilizada em varios metodos
 
-    public function __construct($nome, $host, $usuario, $senha){
+    public function __construct($nome, $host, $usuario, $senha)
+    {
         try {
-            $this->pdo = new PDO("mysql:dbname=".$nome.";host=".$host, $usuario, $senha); //conexao com banco de dados
+            $this->pdo = new PDO("mysql:dbname=" . $nome . ";host=" . $host, $usuario, $senha); //conexao com banco de dados
 
         } catch (PDOException $e) {
-            echo "ERRO NO BANCO DE DADOS ". $e->getMessage(); //caso temha algum erro ao se conectar aparece aqui
+            echo "ERRO NO BANCO DE DADOS " . $e->getMessage(); //caso tenha algum erro ao se conectar aparece aqui
         }
     }
 
-    public function cadastrarProduto($nome, $descricao, $preco, $preco_desconto){ //metodo para cadastrar usuario ao banco
+    public function listarProdutos()
+    {
 
-        $sql = $this->pdo->prepare("SELECT PRODUTO_ID FROM PRODUTO"); //verificando se ja existe o email digitado
-        $sql->execute(); 
+        $sql = $this->pdo->prepare("SELECT PRODUTO_ID, PRODUTO_NOME, PRODUTO_DESC, PRODUTO_PRECO, PRODUTO_DESCONTO, CATEGORIA_ID, PRODUTO_ATIVO 
+        FROM PRODUTO");
+        $sql->execute();
 
-        if($sql -> rowCount() > 0 ){ //verificando se apos executar o SELECT retornou algum usuario no banco
-            return false; //ja esta cadastrado
-        }else{
-            $sql = $this->pdo->prepare("INSERT INTO PRODUTO (PRODUTO_NOME, PRODUTO_DESC, PRODUTO_PRECO, PRODUTO_DESCONTO, PRODUTO_ATIVO)
-            values (:p, :d, :c, :t, 1) ");
-             $sql->bindValue(":p",$nome);
-             $sql->bindValue(":d",$descricao);
-             $sql->bindValue(":c",$preco);
-             $sql->bindValue(":t",$preco_desconto);
-             $sql->execute();
-             return true; // usuario nao existia e foi cadastrado
+        if ($sql->rowCount() > 0) {
+
+            return $sql;
+        } else {
+            echo "Nenhum produto encontrado!";
+        }
+    }
+
+    //------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+    public function cadastrarProduto($nome, $descricao, $preco, $precoDesconto, $produtoAtivo) { 
+
+        $sql = $this->pdo->prepare("INSERT INTO PRODUTO (PRODUTO_NOME, PRODUTO_DESC, PRODUTO_PRECO, PRODUTO_DESCONTO, PRODUTO_ATIVO)
+        VALUES ('$nome', '$descricao', '$preco', '$precoDesconto', '$produtoAtivo')");
+        $sql->execute();
+        return true;
+    }
+
+
+
+    public function listarCategorias()
+    {
+
+        $sql = $this->pdo->prepare("SELECT CATEGORIA_NOME 
+        FROM CATEGORIA");
+        $sql->execute();
+
+        if ($sql->rowCount() > 0) {
+            return $sql;
+        } else {
+            echo "Nenhuma categoria encontrada!";
+
         }
     }
 }
 
-?>
+/*
+public function cadastrarCategorias($categoriaNome, $categoriaDesc, $categoriaAtivo){
+
+    $sql = $this->pdo->prepare("INSERT INTO CATEGORIA (CATEGORIA_NOME, CATEGORIA_DEC, CATEGORIA_ATIVO)")
+    VALUES ('$categoriaNome', '$categoriaDesc', '$categoriaAtivo');
+    $SQL->execute();
+    return true;
+    }
+*/
